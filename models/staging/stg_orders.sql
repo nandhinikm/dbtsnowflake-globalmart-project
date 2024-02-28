d7b85e26cd8218c2f89c3434cdf66c682e4b0804
nandhini3692@gmail.com <nandhini3692@gmail.com>
2024-02-28 16:46:23 +0000

    stg_orders commit

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,24 @@
+select 
+
+--from raw orders
+o.orderid,
+o.orderdate,
+o.shipmode,
+o.shipdate,
+---from raw customer
+c.customername,
+c.segment,
+c.country,
+--from product
+p.category,
+p.productname,
+p.subcategory,
+o.ordersellingprice -o.ordercostprice as orderprofit,
+o.ordersellingprice,
+o.ordercostprice
+ from {{ ref('raw_order') }} as o
+
+left join {{ ref('raw_customer') }} as c
+on o.customerid=c.customerid
+left join {{ ref('raw_product') }} as p
+on o.productid=p.productid
